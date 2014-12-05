9c1e0a632486908d1fafd03abd75400fc2ff1842
villim <villim@126.com>
2014-12-06 00:36:24 +0800

    Sort a linked list

---
+++ src/main/java/io/github/villim/medium/SortLinkedList.java
@@ -0,0 +1,79 @@
+package io.github.villim.medium;
+
+/**
+ * 
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ * 
+ * @author villim
+ *
+ */
+public class SortLinkedList {
+	public ListNode sortList(ListNode head) {
+
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		int length = 0;
+		ListNode currentNode = head;
+		while (currentNode.next != null) {
+			length++;
+			currentNode = currentNode.next;
+		}
+
+		int pivot = length / 2;
+		ListNode pivotHead = head;
+		for (int i = 0; i < pivot; i++) {
+			pivotHead = pivotHead.next;
+		}
+
+		ListNode rightHead = pivotHead.next;
+		pivotHead.next = null;
+
+		return merge(sortList(head), sortList(rightHead));
+	}
+
+	private ListNode merge(ListNode leftHead, ListNode rightHead) {
+
+		ListNode newHead = new ListNode(0);
+		ListNode pointer = newHead;
+		ListNode left = leftHead;
+		ListNode right = rightHead;
+
+		while (left != null && right != null) {
+			if (left.val > right.val) {
+				pointer.next = right;
+				pointer = right;
+				right = right.next;
+			} else {
+				pointer.next = left;
+				pointer = left;
+				left = left.next;
+			}
+		}
+
+		while (left != null) {
+			pointer.next = left;
+			pointer = left;
+			left = left.next;
+		}
+
+		while (right != null) {
+			pointer.next = right;
+			pointer = right;
+			right = right.next;
+		}
+
+		return newHead.next;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
---
+++ src/test/java/io/github/villim/medium/SortLinkedListTest.java
@@ -0,0 +1,41 @@
+package io.github.villim.medium;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SortLinkedListTest {
+	private SortLinkedList classUnderTest = new SortLinkedList();
+
+	ListNode head = null;
+
+	@Before
+	public void setUp() {
+
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = new ListNode(2);
+		node1.next = node2;
+		ListNode node5 = new ListNode(5);
+		node2.next = node5;
+		ListNode node7 = new ListNode(7);
+		node5.next = node7;
+		ListNode node4 = new ListNode(4);
+		node7.next = node4;
+		ListNode node6 = new ListNode(6);
+		node4.next = node6;
+		ListNode node3 = new ListNode(3);
+		node6.next = node3;
+		head = node1;
+
+	}
+
+	@Test
+	public void sort() {
+		ListNode p = classUnderTest.sortList(head);
+		int val = 1;
+		while (p.next != null) {
+			assert (p.val == val++);
+			p = p.next;
+		}
+
+	}
+}
