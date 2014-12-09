7e35cbe21197a71d26d429cca3aa7c84bdf45e60
villim <villim@126.com>
2014-12-09 14:34:51 +0800

    Pascal's Triangle 2

---
+++ src/main/java/io/github/villim/easy/PascalsTriangle2.java
@@ -0,0 +1,64 @@
+package io.github.villim.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an index k, return the kth row of the Pascal's triangle.
+ * 
+ * For example, given k = 3, Return [1,3,3,1].
+ * 
+ * Note: Could you optimize your algorithm to use only O(k) extra space?
+ * 
+ * @author villim
+ *
+ */
+public class PascalsTriangle2 {
+
+	public List<Integer> getRow(int rowIndex) {
+
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		int i = 0;
+		while (i < rowIndex) {
+			row = (ArrayList<Integer>) getNextRow(row);
+			i++;
+		}
+		return row;
+
+	}
+
+	private ArrayList<Integer> getNextRow(List<Integer> previousRow) {
+
+		if (previousRow == null || previousRow.size() == 0) {
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			list.add(1);
+			return list;
+		}
+
+		if (previousRow.size() == 1) {
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			list.add(1);
+			list.add(1);
+			return list;
+		}
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i <= previousRow.size(); i++) {
+			if (i == 0) {
+				list.add(previousRow.get(i));
+				continue;
+			}
+
+			if (i < previousRow.size()) {
+				list.add(previousRow.get(i - 1) + previousRow.get(i));
+			}
+
+			if (i == previousRow.size()) {
+				list.add(previousRow.get(i - 1));
+			}
+		}
+
+		return list;
+	}
+
+}
---
+++ src/test/java/io/github/villim/easy/PascalsTriangle2Test.java
@@ -0,0 +1,53 @@
+package io.github.villim.easy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PascalsTriangle2Test {
+
+	private PascalsTriangle2 classUnderTest = new PascalsTriangle2();
+
+	@Test
+	public void noRow() {
+		assertEquals("[]", classUnderTest.getRow(0).toString());
+	}
+
+	@Test
+	public void oneRow() {
+		assertEquals("[1]", classUnderTest.getRow(1).toString());
+	}
+
+	@Test
+	public void towRow() {
+		assertEquals("[1, 1]", classUnderTest.getRow(2).toString());
+	}
+
+	@Test
+	public void threeRow() {
+		assertEquals("[1, 2, 1]", classUnderTest.getRow(3).toString());
+	}
+
+	@Test
+	public void fourRow() {
+		assertEquals("[1, 3, 3, 1]", classUnderTest.getRow(4).toString());
+	}
+
+	@Test
+	public void fiveRow() {
+		assertEquals("[1, 4, 6, 4, 1]", classUnderTest.getRow(5).toString());
+	}
+
+	@Test
+	public void sixRow() {
+		assertEquals("[1, 5, 10, 10, 5, 1]", classUnderTest.getRow(6)
+				.toString());
+	}
+
+	@Test
+	public void sevenRow() {
+		assertEquals("[1, 6, 15, 20, 15, 6, 1]", classUnderTest.getRow(7)
+				.toString());
+	}
+
+}
