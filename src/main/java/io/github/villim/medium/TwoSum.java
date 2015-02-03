1e1aefa6df3fa690948e0b960237637e0a9b6fe6
Villim Wang <villim@126.com>
2015-02-04 00:02:47 +0800

    add TwoSum

---
+++ src/main/java/io/github/villim/medium/TwoSum.java
@@ -0,0 +1,44 @@
+package io.github.villim.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, find two numbers such that they add up to a
+ * specific target number.
+ * 
+ * The function twoSum should return indices of the two numbers such that they
+ * add up to the target, where index1 must be less than index2. Please note that
+ * your returned answers (both index1 and index2) are not zero-based.
+ * 
+ * You may assume that each input would have exactly one solution.
+ * 
+ * Input: numbers={2, 7, 11, 15}, target=9 Output: index1=1, index2=2
+ * 
+ */
+public class TwoSum {
+	public int[] twoSum(int[] numbers, int target) {
+
+		int[] index = new int[] { -1, -1 };
+
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			map.put(numbers[i], i + 1);
+		}
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] <= target) {
+				Integer theOtherNumberIndex = map.get(target - numbers[i]);
+				if (theOtherNumberIndex != null
+						&& (i + 1) < theOtherNumberIndex) {
+					index[0] = i + 1;
+					index[1] = theOtherNumberIndex;
+				}
+			} else {
+				continue;
+			}
+		}
+
+		return index;
+	}
+}
---
+++ src/test/java/io/github/villim/medium/TwoSumTest.java
@@ -0,0 +1,35 @@
+package io.github.villim.medium;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwoSumTest {
+	private TwoSum classUnderTest = new TwoSum();
+
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void testOrderedArray() {
+		int[] numbers = new int[] { 2, 4, 5, 7 };
+		int[] indexs = this.classUnderTest.twoSum(numbers, 7);
+		assertEquals(1, indexs[0]);
+		assertEquals(3, indexs[1]);
+
+		indexs = this.classUnderTest.twoSum(numbers, 11);
+		assertEquals(2, indexs[0]);
+		assertEquals(4, indexs[1]);
+	}
+
+	@Test
+	public void test0() {
+		int[] numbers = new int[] { 0, 4, 5, 0 };
+		int[] indexs = this.classUnderTest.twoSum(numbers, 0);
+		assertEquals(1, indexs[0]);
+		assertEquals(4, indexs[1]);
+	}
+}
